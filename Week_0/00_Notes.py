7de2b4effc630d5cd699fe4e3b08634cb45ac824
Vid Sarmalkar <vid.sarmalkar@gmail.com>
2024-03-03 18:54:33 -0500

    Initial Commit

---
+++ Week_0/00_Notes.py
@@ -0,0 +1,38 @@
+'''
+Type of data types
+- str
+    # Useful methods of str
+    str.strip()
+- int
+- float
+'''
+
+'''
+# Functions
+    Action, verbs
+    Group of statements
+    - in-built functions
+        functions which Python already knows
+    - user-defined functions
+        functions which user defines and import whenever it is needed
+    - Function if not specified return statement always returns None
+
+# Useful functions
+print()
+    - Parameter inside function call is arguments
+    - Parameter inside function definition is parameters
+    - * means any number of parameters
+    - There are two types of parameters
+        - positional
+        - named
+            - named parameters will have default values
+        - positional parameters always comes first before named
+    - Optional parameters are specified in [] in function definition.
+        - round(num [, number_of_digits])
+
+- Print function
+    - by default will add new line post evaluation of print. Can be changed
+        through "end" named parameter
+    - by default multiple arguments will have space in-between. Can be changed
+        through "sep" named parameter
+'''
